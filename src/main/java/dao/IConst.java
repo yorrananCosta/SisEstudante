17ad0e312b2c3574101e595b5c28044b48ebf43a
yorrananCosta <yorranan.almeida@gmail.com>
2023-02-16 10:28:35 -0300

    #0 - add all files of the project

--- src/main/java/dao/IConst.java
+++ src/main/java/dao/IConst.java
@@ -3,5 +3,5 @@ package dao;
 public interface IConst {
     public static final String stringDeConexao = "jdbc:postgresql://localhost:5432/postgres";
     public static final String usuario = "postgres";
-    public static final String senha = "482593";
+    public static final String senha = "";
 }
